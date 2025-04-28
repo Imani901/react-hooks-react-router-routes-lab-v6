50c72159e80e885124bd01cfbccd6aa11c84509f
faith <faith.henu@student.moringaschool.com>
2025-04-28 08:35:50 +0300

    complete lab

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,13 @@
 import {Link} from 'react-router-dom';
 
-function MovieCard({title}) {
+function MovieCard({title, id}) {
   
   return (
-    <article>
+    <article className='movie-card'>
+        <Link to={`/movies/${id}`}>
         <h2>{title}</h2>
-        {/* What should go here? */}
+        </Link>
+        
     </article>
   );
 };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,23 @@ import "./NavBar.css";
 function NavBar() {
   return (
     <nav className="navbar">
+      <ul>
+        <li>
+          <NavLink to="/" end>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/directors">
+            Directors
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/actors">
+            Actors
+          </NavLink>
+        </li>
+      </ul>
     
     </nav>
     );
